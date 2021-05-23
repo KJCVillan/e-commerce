9767033f535398352b2e0706794bce8d0f9277bd
Kevin Johan Conde <johancondevillan@gmail.com>
2021-05-23 15:30:39 -0500

    terminamos la vista de tienda en tabla y cuadricula

--- src/app/components/productos/productos.component.ts
+++ src/app/components/productos/productos.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Categoria } from 'src/app/entities/categoria';
+import { Producto } from 'src/app/entities/producto';
+import { ProductosService } from 'src/app/services/productos.service';
 
 @Component({
   selector: 'app-productos',
@@ -7,14 +9,28 @@ import { Categoria } from 'src/app/entities/categoria';
   styleUrls: ['./productos.component.css']
 })
 export class ProductosComponent implements OnInit {
-  @Input() CategoriaX: Categoria;
 
-  constructor() { }
+  @Input() CategoriaX: Categoria;
+  listaProductos: Producto[];
+
+  constructor(private productosService: ProductosService) { }
 
   ngOnInit(): void {//se ejecuta una sola vez al cargar el componente
   }
 
   ngOnChanges(): void {//se ejecuta al detectar un cambio
     console.log(this.CategoriaX);
+    this.leerProductos();
+  }
+
+  leerProductos()
+  {
+    const idcategoria = this.CategoriaX.idcategoria;
+    this.productosService.productosSelect(idcategoria).subscribe(
+      (res: Producto[]) => {
+        this.listaProductos = res;
+        console.log(res);
+      }
+    )
   }
 }
--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -1,9 +1,25 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductosService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  productosSelect (idcategoria)
+  {
+    const ruta = "https://servicios.campus.pe/servicioproductos.php";
+
+    const formData: FormData = new FormData();
+    formData.append("caty",idcategoria)
+
+    return this.http.post(ruta,formData).pipe(
+      map((res)=>{
+        return res
+      })
+    )
+  }
 }
